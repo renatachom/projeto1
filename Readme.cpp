2abe92db4ad70bf9b30d7f2cebebe634992d67f4
Renata CHOM <renatachom.ads@gmail.com>
2023-10-08 10:44:09 -0300

    Troca de Variáveis

--- Readme.cpp
+++ Readme.cpp
@@ -5,3 +5,5 @@ complementos. Ter contato com os primeiros programas no curso
 ##  Parte 2 do trabalho programa que peça ao
 usuário 2 números inteiros, A e B, e mostre na tela a soma, subtração, multiplicação,
 divisão e resto da divisão desses números.
+
+## Troca de todas as variáveis do tipo int para float
