b29ee091eea8d4645b182516ea102bfdf2955a7c
Renata CHOM <renatachom.ads@gmail.com>
2023-10-08 10:31:39 -0300

    Parte 2

--- Readme.cpp
+++ Readme.cpp
@@ -1,3 +1,7 @@
 ## Familiarizar-se com a IDE MS-Code. Aprender como compilar e
 executar programas dentro da IDE. Aprender a instalar
 complementos. Ter contato com os primeiros programas no curso
+
+##  Parte 2 do trabalho programa que peça ao
+usuário 2 números inteiros, A e B, e mostre na tela a soma, subtração, multiplicação,
+divisão e resto da divisão desses números.
